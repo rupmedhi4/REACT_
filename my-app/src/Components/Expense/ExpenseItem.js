b5e2e5a543f1f45a0554bca3412df8457a2964e0
Rup medhi <rupmmedhi@gmail.com>
2023-03-04 10:43:55 +0530

    Advance React_Adding Event Listeners made easy_task2

--- my-app/src/Components/Expense/ExpenseItem.js
+++ my-app/src/Components/Expense/ExpenseItem.js
@@ -1,24 +1,34 @@
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
-import ExpenseDetails from './ExpenseDetails';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+import ReactDOM from 'react-dom';
 
+function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("clicked....");
+  };
 
+  const DeleteExpense =() =>{
+    const expenseItemElement = document.querySelector('.expense-item');
+    expenseItemElement.parentNode.removeChild(expenseItemElement);
+    
+  }
 
+  return (
+    <div>
+      <div className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2 className="expense-item h2">{props.title}</h2>
 
-function ExpenseItem(props){
+          <div className="expense-item__price ">{props.amount}</div>
+        </div>
+        <button onClick={clickHandler}>Change title</button>
 
-  
-    return( 
-    <div className="expense-item">
-       <ExpenseDate date={props.date}/>
-       <div className="expense-item__description">
-        <h2 className="expense-item h2">{props.title}</h2>
-       
-        <div className="expense-item__price ">{props.amount}</div>
-        {/* <ExpenseDetails title={props.title}/> */}
-       </div>
+        <button onClick={DeleteExpense}>Delete expense</button>
+      </div>
     </div>
-    )
+  );
 }
 
 export default ExpenseItem;
